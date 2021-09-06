1e46895edb9b1b46b1398de99ac63117f3de82f8
CKautai <80800854+CKautai@users.noreply.github.com>
2021-09-06 12:36:51 +1000

    Add files via upload

---
+++ Week 5/Proof of Concept/my-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+</template>
+
+<script>
+import GameTracking from '.components/GameTracking.Vue'
+
+export default {
+  name: 'App',
+  components: {
+    GameTracking
+  }
+}
+</script>
+
+<style>
+
+  #app {
+      box-sizing: border-box;
+      font-family: 'Courier New', monospace;
+  }
+</style>
